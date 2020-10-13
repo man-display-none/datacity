0e6ca3843826dcc84f2a3c0f962cedd484bd7118
James Mannarino <jamesmannarino@gmail.com>
2020-10-13 09:19:30 -0400

    feat: draft calc in BuildingInfo popup component

--- client/components/BuildingInfo.js
+++ client/components/BuildingInfo.js
@@ -1,49 +1,70 @@
 import React from 'react'
-import {connect} from 'react-redux'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
+import Sidebar from 'react-sidebar'
 
-export class BuildingInfo extends React.Component {
+export default class BuildingInfo extends React.Component {
   constructor() {
     super()
     this.state = {
-      showModal: true
+      showModal: true,
+      calculatedECost: 0
     }
     this.handleClose = this.handleClose.bind(this)
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
   componentDidMount() {
-    this.props.moreInfo()
+    // this.props.moreInfo()
+    this.setState({
+      calculatedECost: this.props.info.totalEnergyCost
+    })
+  }
+  onSetSidebarOpen(open) {
+    this.setState({sidebarOpen: open})
   }
   handleClose() {
     this.setState({showModal: !this.state.showModal})
   }
+  handleClick() {
+    this.setState({
+      calculatedECost: this.props.info.lightingImprovement()
+    })
+  }
   render() {
+    console.log(this.props.info)
+    const {
+      bbl,
+      electricityUse,
+      fuelUse,
+      ghgEmissions,
+      score,
+      totalEnergyCost,
+      waterUse
+    } = this.props.info
     return (
       <div>
         <Modal show={this.state.showModal} onHide={this.handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Building Information</Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <div className="buildingData">
+              <p>Building id: {bbl}</p>
+              <p>EnergyStar score: {score}</p>
+              <p>Annual electricity usage: {electricityUse.toFixed(0)} kWh</p>
+              <p>Annual fuel usage: {fuelUse.toFixed(0)} MCF</p>
+              <p>Annual water usage: {waterUse.toFixed(0)} Gallons</p>
+              <p>Annual GHG emissions: {ghgEmissions.toFixed(0)} MTCO2e</p>
               <p>
-                {/*building information
-            property Type
-            property Size
-            year built
-
-          Energy usage:
-            energy star score
-            fuel type
-            energy usage
-            normalized usage
-            greenhouse gas emissions
-
-       */}
+                Annual energy cost: ${this.state.calculatedECost.toFixed(0)}
               </p>
             </div>
           </Modal.Body>
           <Modal.Footer>
+            <Button variant="primary" onClick={this.handleClick}>
+              Upgrade Lighting
+            </Button>
             <Button variant="secondary" onClick={this.handleClose}>
               Close
             </Button>
@@ -53,13 +74,3 @@ export class BuildingInfo extends React.Component {
     )
   }
 }
-
-const mapState = state => {
-  return {
-    moreInfo: state.buildData
-  }
-}
-
-const mapDispatch = dispatch => {}
-
-export default connect(mapState, mapDispatch)(BuildingInfo)
--- client/components/landing-page.js
+++ client/components/landing-page.js
@@ -1,32 +1,110 @@
-import React, {Component} from 'react'
-const mapboxgl = require('mapbox-gl')
+import React, {useRef, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import mapboxgl from 'mapbox-gl'
+import BuildingInfo from './BuildingInfo'
+import Building from '../calculator'
 
 //hide access token
 mapboxgl.accessToken =
   'pk.eyJ1IjoiamVmZi0wMjI4IiwiYSI6ImNrZzZ4ZW5kbzAxc2cydG16a2syZWh5eW4ifQ.AFSJlXJOrlrnjsLHBCfpbw'
 
-export default class LandingPage extends Component {
-  componentDidMount() {
+const LandingPage = () => {
+  const mapContainerRef = useRef(null)
+  const popUpRef = useRef(new mapboxgl.Popup({offset: 15}))
+  useEffect(() => {
     const map = new mapboxgl.Map({
-      container: this.mapContainer,
+      container: mapContainerRef.current,
       style: 'mapbox://styles/jeff-0228/ckg34hbar1c8i19s5gth69huy',
       center: [-74.0066, 40.7135],
       zoom: 13
     })
 
     map.on('load', function() {
-      map.on('click', 'footprint', function(e) {
+      map.on('click', 'footprint', async function(e) {
+        const bbl = e.features[0].properties.base_bbl
+        const lat = e.features[0].geometry.coordinates[0][0]
+        const long = e.features[0].geometry.coordinates[0][1]
+
+        const {data: bldg} = await axios.get(
+          `https://data.cityofnewyork.us/resource/28fi-3us3.json?bbl_10_digits=${bbl}`
+        )
+        //some multiple bbl results
+        // console.log(bldg)
+        const {
+          electricity_use_grid_purchase,
+          electricity_use_grid_purchase_1,
+          fuel_oil_1_use_kbtu,
+          fuel_oil_2_use_kbtu,
+          fuel_oil_4_use_kbtu,
+          fuel_oil_5_6_use_kbtu,
+          natural_gas_use_kbtu,
+          kerosene_use_kbtu,
+          diesel_2_use_kbtu,
+          total_ghg_emissions_metric,
+          water_use_all_water_sources,
+          energy_star_score,
+          bbl_10_digits
+        } = bldg[0]
+
+        //check if values are 'number strings' or not and then add
+        const totalUse = arr => {
+          let total = 0
+          arr.forEach(use => {
+            if (!isNaN(Number(use))) {
+              total += Number(use)
+            }
+          })
+          return total
+        }
+
+        const totalElectricity = totalUse([
+          electricity_use_grid_purchase,
+          electricity_use_grid_purchase_1
+        ])
+
+        const totalFuel = totalUse([
+          fuel_oil_1_use_kbtu,
+          fuel_oil_2_use_kbtu,
+          fuel_oil_4_use_kbtu,
+          fuel_oil_5_6_use_kbtu,
+          natural_gas_use_kbtu,
+          kerosene_use_kbtu,
+          diesel_2_use_kbtu
+        ])
+
+        const totalWater = Number(water_use_all_water_sources)
+
+        const ghg = Number(total_ghg_emissions_metric)
+
+        const building = new Building(
+          totalElectricity,
+          totalFuel,
+          totalWater,
+          ghg,
+          energy_star_score,
+          bbl_10_digits
+        )
+
         //use e.features[0].properties.base_bbl for axios calls
-        console.log('e.features --->', e.features[0].properties)
+        const popupNode = document.createElement('div')
+        const showInfo = ReactDOM.render(
+          <BuildingInfo info={building} />,
+          popupNode
+        )
+
+        popUpRef.current
+          .setLngLat(lat, long)
+          .setDOMContent(showInfo)
+          .addTo(map)
       })
     })
-  }
-
-  render() {
-    return (
-      <div>
-        <div ref={el => (this.mapContainer = el)} className="mapContainer" />
-      </div>
-    )
-  }
+  })
+  return (
+    <div>
+      <div ref={mapContainerRef} className="mapContainer" />
+    </div>
+  )
 }
+
+export default LandingPage
