21cf62a8ab743510d7cfd33cdf1fdec8bdfcec89
dhuang612 <dhuang@madisoncollege.edu>
2020-10-15 18:33:01 -0400

    feat: used react hooks to interact with redux

--- client/components/landing-page.js
+++ client/components/landing-page.js
@@ -1,7 +1,6 @@
 import React, {useRef, useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import ReactDOM from 'react-dom'
-import axios from 'axios'
 import mapboxgl from 'mapbox-gl'
 import BuildingInfo from './BuildingInfo'
 import ColorKey from './ColorKey'
@@ -32,79 +31,76 @@ const LandingPage = props => {
         // const lat = e.features[0].geometry.coordinates[0][0]
         // const long = e.features[0].geometry.coordinates[0][1]
         const lngLat = e.lngLat
-        const {data: bldg} = await axios.get(
-          `https://data.cityofnewyork.us/resource/28fi-3us3.json?bbl_10_digits=${bbl}`
-        )
         //some multiple bbl results need to be accounted for
         // console.log(bldg)
+        const buildInfo = await dispatch(updatedInfo(bbl))
+        if (buildInfo) {
+          const {
+            electricity_use_grid_purchase,
+            electricity_use_grid_purchase_1,
+            fuel_oil_1_use_kbtu,
+            fuel_oil_2_use_kbtu,
+            fuel_oil_4_use_kbtu,
+            fuel_oil_5_6_use_kbtu,
+            natural_gas_use_kbtu,
+            kerosene_use_kbtu,
+            diesel_2_use_kbtu,
+            total_ghg_emissions_metric,
+            water_use_all_water_sources,
+            energy_star_score,
+            bbl_10_digits
+          } = buildInfo.buildData
 
-        const {
-          electricity_use_grid_purchase,
-          electricity_use_grid_purchase_1,
-          fuel_oil_1_use_kbtu,
-          fuel_oil_2_use_kbtu,
-          fuel_oil_4_use_kbtu,
-          fuel_oil_5_6_use_kbtu,
-          natural_gas_use_kbtu,
-          kerosene_use_kbtu,
-          diesel_2_use_kbtu,
-          total_ghg_emissions_metric,
-          water_use_all_water_sources,
-          energy_star_score,
-          bbl_10_digits
-        } = bldg[0]
+          //dispatch(updatedInfo())
+          //check if values are 'number strings' or not and then add
+          const totalUse = arr => {
+            let total = 0
+            arr.forEach(use => {
+              if (!isNaN(Number(use))) {
+                total += Number(use)
+              }
+            })
+            return total
+          }
 
-        console.log(dispatch(updatedInfo(bbl)))
+          const totalElectricity = totalUse([
+            electricity_use_grid_purchase,
+            electricity_use_grid_purchase_1
+          ])
 
-        //dispatch(updatedInfo())
-        //check if values are 'number strings' or not and then add
-        const totalUse = arr => {
-          let total = 0
-          arr.forEach(use => {
-            if (!isNaN(Number(use))) {
-              total += Number(use)
-            }
-          })
-          return total
+          const totalFuel = totalUse([
+            fuel_oil_1_use_kbtu,
+            fuel_oil_2_use_kbtu,
+            fuel_oil_4_use_kbtu,
+            fuel_oil_5_6_use_kbtu,
+            natural_gas_use_kbtu,
+            kerosene_use_kbtu,
+            diesel_2_use_kbtu
+          ])
+
+          const totalWater = Number(water_use_all_water_sources)
+
+          const ghg = Number(total_ghg_emissions_metric)
+
+          const building = new Building(
+            totalElectricity,
+            totalFuel,
+            totalWater,
+            ghg,
+            energy_star_score,
+            bbl_10_digits
+          )
+
+          //use e.features[0].properties.base_bbl for axios calls
+          const popupNode = document.createElement('div')
+          const showInfo = ReactDOM.render(
+            <BuildingInfo info={building} />,
+
+            popupNode
+          )
+
+          markerRef.current.setLngLat([lngLat.lng, lngLat.lat]).addTo(map)
         }
-
-        const totalElectricity = totalUse([
-          electricity_use_grid_purchase,
-          electricity_use_grid_purchase_1
-        ])
-
-        const totalFuel = totalUse([
-          fuel_oil_1_use_kbtu,
-          fuel_oil_2_use_kbtu,
-          fuel_oil_4_use_kbtu,
-          fuel_oil_5_6_use_kbtu,
-          natural_gas_use_kbtu,
-          kerosene_use_kbtu,
-          diesel_2_use_kbtu
-        ])
-
-        const totalWater = Number(water_use_all_water_sources)
-
-        const ghg = Number(total_ghg_emissions_metric)
-
-        const building = new Building(
-          totalElectricity,
-          totalFuel,
-          totalWater,
-          ghg,
-          energy_star_score,
-          bbl_10_digits
-        )
-
-        //use e.features[0].properties.base_bbl for axios calls
-        const popupNode = document.createElement('div')
-        const showInfo = ReactDOM.render(
-          <BuildingInfo info={building} />,
-
-          popupNode
-        )
-
-        markerRef.current.setLngLat([lngLat.lng, lngLat.lat]).addTo(map)
       })
     })
     console.log('this.props', props)
